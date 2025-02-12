15a593d24d70b9d22743e72eb0278b3b1d3830aa
mohammed <eng.mohammed.mamdouh193@gmail.com>
2025-02-12 16:25:15 +0300

    v4

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.delete(`/api/${COLLECTION_NAME}/:id`, async (req, res) => {
 
 // Root Route
 app.get('/', (req, res) => {
-    res.send('Welcome to the Redayit V3 App!');
+    res.send('Welcome to the Redayit V4 App!');
 });
 
 // Start Server
